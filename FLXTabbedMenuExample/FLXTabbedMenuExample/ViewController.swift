e9a4dcaaca418abc33504a7a79be0b2cd14ee246
Mark Jarecki <markjarecki@fluxom.com>
2019-05-17 11:18:06 +0200

    Add - initial root view controller

--- FLXTabbedMenuExample/FLXTabbedMenuExample/ViewController.swift
+++ FLXTabbedMenuExample/FLXTabbedMenuExample/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
+    
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Basic view properties
+        view.backgroundColor = .white
+
     }
 
 
