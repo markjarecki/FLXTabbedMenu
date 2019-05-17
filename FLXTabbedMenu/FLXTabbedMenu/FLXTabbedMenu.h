e5402e0602894e489a114d702041d69703186819
Mark Jarecki <markjarecki@fluxom.com>
2019-05-17 11:13:45 +0200

    Add - Initial files

---
+++ FLXTabbedMenu/FLXTabbedMenu/FLXTabbedMenu.h
@@ -0,0 +1,19 @@
+//
+//  FLXTabbedMenu.h
+//  FLXTabbedMenu
+//
+//  Created by Mark Jarecki on 17/5/19.
+//  Copyright © 2019 Mark Jarecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FLXTabbedMenu.
+FOUNDATION_EXPORT double FLXTabbedMenuVersionNumber;
+
+//! Project version string for FLXTabbedMenu.
+FOUNDATION_EXPORT const unsigned char FLXTabbedMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLXTabbedMenu/PublicHeader.h>
+
+
---
+++ FLXTabbedMenuExample/FLXTabbedMenuExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FLXTabbedMenuExample
+//
+//  Created by Mark Jarecki on 17/5/19.
+//  Copyright © 2019 Mark Jarecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
